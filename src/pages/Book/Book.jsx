014c872ecd255f6ff4bf8093d874507fdfb7e862
mahib-bit <sadmanmahib@gmail.com>
2026-09-03 02:08:59 +0600

    completed book cards

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -8,20 +8,27 @@ const Book = ({ singleBook }) => {
     const { bookName, author, image, review, rating, category, tags } = singleBook;
 
     return (
-        
-        <div className=" card bg-base-100 w-96 shadow-sm border border-mist-500 p-6">
-            <figure className='bg-mist-500 rounded-xl'>
-                <img
+                               
+        <div className=" card bg-base-100 w-full h-full shadow-sm border border-mist-500 p-6">
+            <figure className='w-full h-96 bg-mist-600 rounded-xl'>
+                <img className='w-60 h-85 object-contain'
                     src={image} />
             </figure>
             <div className="card-body">
-                <div className="badge text-lime-400 border-lime-200">{tags}</div>
-                <h2 className="card-title">
+                <div className='flex justify-center gap-2 items-end text-lime-400'>
+                    {
+                        singleBook.tags.map((tag, index) => (
+                            <p className='border px-3 rounded-xl ' key={index}>{tag}</p>
+                        ))
+                    }
+                </div>
+                {/* <div className="badge text-lime-400 border-lime-200">{tags}</div> */}
+                <h2 className="card-title text-2xl font-bold">
                     {bookName}
                 </h2>
-                <p>By : {author}</p>
+                <p className='text-[16px] font-medium'>By : {author}</p>
                 <hr/>
-                <div className=" card-actions items-center justify-between">
+                <div className=" text-[16px] card-actions items-center justify-between">
                     <div className="">{category}</div>
                     <div className="">{rating} ⭐</div>
                 </div>
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -20,7 +20,7 @@ const Books = ({ data }) => {
         <div>
             <h1 className='text-3xl text-center font-bold'>Books</h1>
             <Suspense fallback={<span>Loading...</span>}>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-between'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-between p-6 gap-6'>
                     {
                         data.map((singleBook) =>
                             <Book key={singleBook.bookId} singleBook={singleBook}>
