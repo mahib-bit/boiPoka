5c6c7adf0d95a81048d8bd6659be3e3d9b0ce577
mahib-bit <sadmanmahib@gmail.com>
2026-09-02 23:27:04 +0600

    useEffect

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Suspense, useEffect } from 'react';
+import { useState } from 'react';
+import Book from '../Book/Book';
 
 const Books = () => {
+    const [allBooks,setAllBooks] = useState([]);
+
+    // useEffect(() => {
+    //     fetch("booksData.json")
+    //     .then(res => res.json)
+    //     .then(data => {
+    //         setAllBooks(data)
+    //     })
+    // },[] )
+    
+    const bookPromise = fetch('./booksData.json')
+    .then(res => res.json())
+
     return (
         <div>
-            <h1>Boooooksssss</h1>
+            <h1 className='text-3xl text-center font-bold'>Books</h1>
+            <Suspense fallback={<span>Loading...</span>}>
+                {
+                    <Book bookPromise={bookPromise}></Book>
+                }
+            </Suspense>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Books from '../Books/Books';
 
 const Home = () => {
-    const [allBooks,setAllBooks] = useState([]);
 
-    useEffect(() => {
-        fetch("booksData.json")
-        .then(res => res.json)
-        .then(data => {
-            setAllBooks(data)
-        })
-    },[] )
-    
     return (
         <div>
             <Banner></Banner>
