d0a818ca2e64c1529176281ef60d4d55d2f24afa
mahib-bit <sadmanmahib@gmail.com>
2026-09-02 21:22:55 +0600

    header footer and setups

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className='flex justify-center items-center my-6 mx-auto, font-bold text-6xl'>404 Page not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
