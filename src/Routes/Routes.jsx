65c47897d3cfb0b503456a58e7190911a6b2028d
mahib-bit <sadmanmahib@gmail.com>
2026-09-03 04:47:16 +0600

    dynamic route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Root from '../pages/Root/Root';
 import ErrorPage from '../pages/ErrorPage/ErrorPage'
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
+import BookDetails from '../pages/BookDetails/BookDetails';
 
 export const router = createBrowserRouter([
   {
@@ -22,7 +23,8 @@ export const router = createBrowserRouter([
         Component: About,
       },
       {
-        path : ''
+        path : '/details/:id',
+        Component : BookDetails,
       }
     ]
   }
--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,40 +1,43 @@
 import { Star } from 'lucide-react';
 import React, { use } from 'react';
+import { Link } from 'react-router';
 
 const Book = ({ singleBook }) => {
 
     // const data=use(bookPromise);
     // console.log(data)
 
-    const { bookName, author, image, review, rating, category, tags } = singleBook;
+    const { bookId, bookName, author, image, rating, category, tags } = singleBook;
 
     return (
-                               
-        <div className=" card bg-base-100 w-full h-full shadow-sm border border-mist-500 p-6">
-            <figure className='w-full h-96 bg-mist-500 rounded-xl'>
-                <img className='w-60 h-85 object-contain'
-                    src={image} />
-            </figure>
-            <div className="card-body">
-                <div className='flex justify-center shadow-2xl gap-2 items-end text-gray-400'>
-                    {
-                        singleBook.tags.map((tag, index) => (
-                            <button className='border font-medium text-lime-400 px-3 mr-2 rounded-xl ' key={index}>{tag}</button>
-                        ))
-                    }
-                </div>
-                {/* <div className="badge text-lime-400 border-lime-200">{tags}</div> */}
-                <h2 className="card-title text-2xl font-bold">
-                    {bookName}
-                </h2>
-                <p className='text-[16px] font-medium'>By : {author}</p>
-                <hr className='border-dashed'/>
-                <div className=" text-[16px] card-actions items-center justify-between">
-                    <div className="">{category}</div>
-                    <div className="flex items-center justify-center gap-1">{rating}<Star></Star></div>
+        <Link to={`/details/${bookId}`}>
+            <div className=" card bg-base-100 w-full h-full shadow-sm border border-mist-500 p-6">
+                <figure className='w-full h-96 bg-mist-500 rounded-xl'>
+                    <img className='w-60 h-85 object-contain'
+                        src={image} />
+                </figure>
+                <div className="card-body">
+                    <div className='flex justify-center shadow-2xl gap-2 items-end text-gray-400'>
+                        {
+                            tags.map((tag, index) => (
+                                <button className='border font-medium text-lime-400 px-3 mr-2 rounded-xl ' key={index}>{tag}</button>
+                            ))
+                        }
+                    </div>
+                    {/* <div className="badge text-lime-400 border-lime-200">{tags}</div> */}
+                    <h2 className="card-title text-2xl font-bold">
+                        {bookName}
+                    </h2>
+                    <p className='text-[16px] font-medium'>By : {author}</p>
+                    <hr className='border-dashed' />
+                    <div className=" text-[16px] card-actions items-center justify-between">
+                        <div className="">{category}</div>
+                        <div className="flex items-center justify-center gap-1">{rating}<Star></Star></div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
+
     );
 };
 
